69ac079033e6cb3032eaa3fd7c4db0ef4ddb832f
Kaj Dreef <kaj.dreef@gmail.com>
2020-09-20 16:49:27 -0700

    Able to run the experiment in a docker container.

--- spidertools/runners/docker_cli.py
+++ spidertools/runners/docker_cli.py
@@ -1,8 +1,10 @@
+import argparse
 import tarfile
 import docker
 import logging
 from spidertools.utils.analysis_repo import AnalysisRepo
 import xml
+from typing import Dict
 
 
 def get_jdk_version(repo: AnalysisRepo):
@@ -11,13 +13,35 @@ def get_jdk_version(repo: AnalysisRepo):
 def create_archive(directory):
     pass
 
+def parse_arguments():
+    """
+    Parse arguments to run experiment in docker container.
+    """
+    parser = argparse.ArgumentParser(description="Tool to run the spidertools analysis framework in a docker container")
+
+    parser.add_argument('project_url', type=str, help="URL to a git repository that can be cloned for analysis")
+    parser.add_argument('output_dir', type=str, help="Directory where the user wants to store the output from the analysis")
+    
+    return parser.parse_args()
+
+def create_volume(host_dir: str) -> Dict:
+    if host_dir is None:
+        raise Exception("Host or docker target directory is undefined...")
+
+    return {
+        host_dir: {
+            'bind': "/home/spiderlab/data/",
+            'mode': 'rw'
+        }
+    }
+
 def main():
     # parse arguments
     #   - project url
     #   - database path
-    #   - overwrite jdk?
-
-    url = 'https://github.com/apache/commons-io'
+    args = parse_arguments()
+    url = args.project_url
+    output_dir = args.output_dir
 
     repo = AnalysisRepo(url)
 
@@ -36,7 +60,11 @@ def main():
     docker_image_tag = map_jdk_to_image[jdk_version]
 
     # Make connection with the docker deamon 
-    client = docker.from_env()
+    try: 
+        client = docker.from_env()
+    except Exception:
+        print(f"[ERROR] Docker is not installed or not running at the moment...")
+        exit(1)
 
     try: 
         client.images.get(docker_image_tag)
@@ -44,14 +72,18 @@ def main():
         print(f"[ERROR] Docker image '{docker_image_tag}' was not found...")
         exit(1)
 
+    # Create volume to use with the docker container:
+    volume = create_volume(output_dir)
+
     # Build command
     cmd = [
         "pluperfect",
         url,
+        '--current',
         "--config .spider.yml"
     ]
 
     print(f"[INFO] cmd: {' '.join(cmd)}")
 
     # Start docker image with database path volume
-    client.containers.run(docker_image_tag, ' '.join(cmd), stderr=True)
+    client.containers.run(docker_image_tag, ' '.join(cmd), volumes=volume, stderr=True)
