aca9ceb9d6db3ce9c84b5effa1936096d9f25757
Kaj Dreef <kaj.dreef@gmail.com>
2020-10-12 18:26:40 -0700

    WIP sorting using machine learning

--- spidertools/data/sorting.py
+++ spidertools/data/sorting.py
@@ -1,6 +1,8 @@
 from typing import List, Dict
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import pairwise_distances
+from scipy.sparse import dok_matrix
+import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
@@ -30,6 +32,39 @@ def test_name(coverage: Dict, reverse=False) -> Dict:
     return coverage
 
 
-def cluster(coverage: Dict, threshold=0.1) -> Dict:
-    logger.warn("Not implemented yet...")
+def cluster_tests(coverage: Dict, threshold=0.3) -> Dict:
+
+    tests = coverage["tests"]
+    methods = coverage["methods"]
+    links = coverage["links"]
+
+    test_id_map = dict()
+    for i, test in enumerate(tests):
+        if 'test_id' in test:
+            test_id_map[test['test_id']] = i
+        else:
+            print(test)
+    
+    method_id_map = dict()
+    for i, method in enumerate(methods):
+        if 'method_id' in method:
+            method_id_map[method['method_id']] = i
+        else:
+            print(method)
+
+    sparse = dok_matrix((len(methods) + 1, len(tests) + 1), dtype=np.int)
+    sparse.transpose()
+    for link in links:
+        try:
+            sparse[method_id_map[link['method_id']], test_id_map[link['test_id']]] = 1
+        except Exception as e:
+            print(e)
+
+    method_labels = AgglomerativeClustering(
+        n_clusters=None,
+        affinity='euclidean',
+    ).fit_predict(sparse.todense())
+
+    unique = np.unique(method_labels)
+
     return coverage
