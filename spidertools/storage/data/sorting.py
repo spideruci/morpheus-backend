faf4a391ad13b8b755387abe4173b164f315b20d
Kaj Dreef <kaj.dreef@gmail.com>
2020-10-28 23:33:41 -0700

    Implemented clustering, now need to create a public api to actually access it.

--- spidertools/storage/data/sorting.py
+++ spidertools/storage/data/sorting.py
@@ -4,6 +4,7 @@ from sklearn.metrics import pairwise_distances
 from scipy.sparse import dok_matrix
 import numpy as np
 import logging
+from pprint import pprint
 
 logger = logging.getLogger(__name__)
 
@@ -31,12 +32,34 @@ def test_name(coverage: Dict, reverse=False) -> Dict:
     )
     return coverage
 
+def cluster_methods(coverage: Dict, threshold=0.2) -> Dict:
+    methods = coverage["methods"]
+    cluster_labels = __clustering(coverage, threshold, transpose=False)
 
-def cluster_tests(coverage: Dict, threshold=0.3) -> Dict:
+    for method, cluster_id in zip(methods, cluster_labels):
+        method["cluster_id"] = cluster_id.item()
+
+    coverage['methods'] = __multi_key_sort(methods, ['cluster_id', 'package_name', 'class_name', 'method_name'])
+
+    return coverage
+
+def cluster_tests(coverage: Dict, threshold=0.2) -> Dict:
+    coverage = name(coverage)
 
     tests = coverage["tests"]
+    cluster_labels = __clustering(coverage, threshold, transpose=True)
+    
+    for test, cluster_id in zip(tests, cluster_labels):
+        test["cluster_id"] = cluster_id.item()
+
+    coverage['tests'] = __multi_key_sort(tests, ['cluster_id', 'class_name', 'method_name'])
+
+    return coverage
+
+def __clustering(coverage: Dict, threshold, transpose=False) -> Dict:
     methods = coverage["methods"]
-    links = coverage["links"]
+    tests = coverage["tests"]
+    edges = coverage["edges"]
 
     test_id_map = dict()
     for i, test in enumerate(tests):
@@ -52,19 +75,26 @@ def cluster_tests(coverage: Dict, threshold=0.3) -> Dict:
         else:
             print(method)
 
-    sparse = dok_matrix((len(methods) + 1, len(tests) + 1), dtype=np.int)
-    sparse.transpose()
-    for link in links:
+    sparse = dok_matrix((len(methods), len(tests)), dtype=np.int)
+    if transpose:
+        sparse = sparse.transpose()
+
+    for edge in edges:
         try:
-            sparse[method_id_map[link['method_id']], test_id_map[link['test_id']]] = 1
+            sparse[method_id_map[edge['method_id']], test_id_map[edge['test_id']]] = 1
         except Exception as e:
             print(e)
 
-    method_labels = AgglomerativeClustering(
+    return AgglomerativeClustering(
         n_clusters=None,
         affinity='euclidean',
+        distance_threshold=threshold,
     ).fit_predict(sparse.todense())
 
-    unique = np.unique(method_labels)
-
-    return coverage
+def __multi_key_sort(objects: List, keys: List[str], reverse=False):
+    objects = sorted(
+        objects,
+        key=lambda x : tuple([x[key] for key in keys]),
+        reverse=reverse
+    )
+    return objects
--- tests/integration/test_sorting.py
+++ tests/integration/test_sorting.py
@@ -1,35 +1,75 @@
 import unittest
+import pytest
+import json
 import os
-from spidertools.parsing_data.coverage_json import coverage_json
-from spidertools.storage.table_handlers import ProjectTableHandler, CommitTableHandler, MethodCoverageHandler
-from spidertools.data.sorting import cluster_tests
+from typing import List, Dict
+from flask import jsonify
+from spidertools.storage.parsing.tacoco import TacocoParser
+from spidertools.storage.parsing.methods import MethodParser
+from spidertools.storage.models.repository import Commit, Project
+from spidertools.storage.db_helper import DatabaseHelper
+from spidertools.storage.query.querybuilder import MethodCoverageQuery
+from spidertools.storage.query.output_formatter import coverage_format
+from spidertools.storage.data.sorting import cluster_tests
 
-class TestSortingData(unittest.TestCase):
+class TestQueryCoverageData(unittest.TestCase):
+
     def setUp(self):
-        DB_PATH = ':memory:'
-        project_handler = ProjectTableHandler(DB_PATH)
-        commit_handler = CommitTableHandler(DB_PATH)
-        coverage_handler = MethodCoverageHandler(DB_PATH)
-        resource_dir = f'.{os.path.sep}tests{os.path.sep}resources{os.path.sep}'
+        self.db_helper = DatabaseHelper(':memory:')
+        self.resource_dir = f'.{os.path.sep}tests{os.path.sep}resources{os.path.sep}'
+
+    def tearDown(self):
+        pass
+
+    def __init_database(self, project, commit):
+        method_parser : MethodParser = MethodParser()
+        methods_dict: List[Dict] = self.__load_methods(project.project_name, commit.sha)
+        methods = method_parser\
+            .set_commit(commit)\
+            .parse(methods_dict)
         
-        # Given:    
-        project_name = "jsoup"
-        commit_sha = "2a2e8421c2aca89a21c786aa85af215dcbde02d7"
-        coverage_matrix = f'{resource_dir}{project_name}{os.path.sep}{commit_sha}-cov-matrix.json'
-        methods = f'{resource_dir}{project_name}{os.path.sep}methods-{commit_sha}.json'
+        method_parser.store(self.db_helper, project, commit, methods)
+        
+        tacoco_parser : TacocoParser = TacocoParser()
+        coverage_dict: Dict = self.__load_coverage(project.project_name, commit.sha)
+        coverage = tacoco_parser\
+            .parse(coverage_dict)
 
-        # When:
-        output = coverage_json(methods, coverage_matrix, commit_sha)
+        tacoco_parser.store(self.db_helper, project, commit, coverage)
 
-        project_id = project_handler.add_project(project_name)
-        commit_id = commit_handler.add_commit(project_id, commit_sha)
-        coverage_handler.add_project_coverage(project_id, commit_id, output["methods"]["production"], output["methods"]["test"])
+    def __load_coverage(self, project_name, commit_sha) -> List[Dict]:
+        coverage_matrix_file = f'{self.resource_dir}{project_name}{os.path.sep}{commit_sha}-cov-matrix.json'
+        with open(coverage_matrix_file) as coverage:
+            return json.load(coverage)
 
-        # Then:
-        self.output = coverage_handler.get_project_coverage(commit_id)
+    def __load_methods(self, project_name, commit_sha) -> List[Dict]:
+        methods_file = f'{self.resource_dir}{project_name}{os.path.sep}methods-{commit_sha}.json'
+        with open(methods_file) as methods:
+            return json.load(methods)
 
-    def test_sorting_data(self):
-        output = cluster_tests(self.output)
+    def test_querying_coverage_primitive_hamcrest(self):
+        # Given: an initialized database
+        project_name = 'primitive-hamcrest'
+        commit_sha = '250f63fe6e70ca6c44ee696c1937b5ccb14f2e6e'
 
-        assert output["links"] is not None
-        assert len(output["links"]) != 0
+        project = Project(project_name=project_name)
+        commit = Commit(sha=commit_sha)
+
+        self.__init_database(project, commit)
+
+        # When: we query a specific method
+        with self.db_helper.create_session() as session:
+            query = MethodCoverageQuery(session)\
+                .set_commit(commit)\
+                .set_project(project)
+
+            methods = query.get_methods()
+            tests = query.get_tests()
+            edges = query.get_coverage()
+
+        coverage = coverage_format(methods, tests, edges)
+
+        coverage = cluster_tests(coverage)
+
+        assert coverage is not None
+        assert json.dumps(coverage) is not None
